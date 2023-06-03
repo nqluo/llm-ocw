ce099258377a9bc3915c101ae3793398daf02b46
Ben Luo <18134731+nqluo@users.noreply.github.com>
2023-06-03 23:10:33 +0800

    add a copy of Jupyter notebook from ChatGPT Prompt Engineering for Developers

---
+++ ChatGPT_Prompt_Engineering_for_Developers/chatbot/01 chatgpt simple.py
@@ -0,0 +1,10 @@
+import os
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
+print(completion.choices[0].message.content)
